6ac8e57d84721cdf9e1a74db29374623f50c0ebf
dymafr <erwan1309@hotmail.com>
2022-03-16 16:23:19 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
-  <form>
+  <form @submit="mySubmit">
     <div>
       <input v-model="usernameValue" type="text" placeholder="Prénom" />
     </div>
-    <pre>
- {{ errors }}
-    </pre>
-
-    <button type="button" @click="triggerErreur()">erreur</button>
-    <button type="button" @click="triggerValue()">valeur</button>
+    <button>Envoi</button>
   </form>
 </template>
 
@@ -17,32 +12,21 @@ import { useField, useForm } from 'vee-validate';
 import { z } from 'zod';
 import { toFieldValidator } from '@vee-validate/zod';
 
-const {
-  errors: formErrors,
-  setFieldValue,
-  setValues,
-  setFieldError,
-  setErrors: setFormErrors,
-} = useForm();
+const { handleSubmit } = useForm();
 
-const {
-  value: usernameValue,
-  errorMessage,
-  errors,
-  setValue,
-  setErrors,
-} = useField(
+const mySubmit = handleSubmit(
+  (values, actions) => {
+    console.log(values);
+  },
+  (errors) => {
+    console.log(errors);
+  }
+);
+
+const { value: usernameValue, errorMessage } = useField(
   'username',
   toFieldValidator(z.string().min(5, { message: 'Trop court !' }))
 );
-
-function triggerErreur() {
-  setErrors('oops');
-}
-
-function triggerValue() {
-  setValue('test');
-}
 </script>
 
 <style scoped lang="scss"></style>
